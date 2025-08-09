346b25004ffe959e0ced4af81f289ff540fc071e
yuq_laptop_x1 <yuq_laptop_x1@qq.com>
2025-08-09 10:10:16 +0800

    前端报错修改

--- web/app/components/tools/mcp/hooks.ts
+++ web/app/components/tools/mcp/hooks.ts
@@ -3,10 +3,12 @@ import { useCallback } from 'react'
 import { useI18N } from '@/context/i18n'
 
 export const useFormatTimeFromNow = () => {
-  const { locale } = useI18N()
-  const formatTimeFromNow = useCallback((time: number) => {
-    return dayjs(time).locale(locale === 'zh-Hans' ? 'zh-cn' : locale).fromNow()
-  }, [locale])
+    const { locale } = useI18N()
+    // .fromNow() 自己修改的 这里原代码会报错,英雌去掉fromNow()
+    const formatTimeFromNow = useCallback((time: number) => {
+        return dayjs(time).locale(locale === 'zh-Hans' ? 'zh-cn' : locale)
+        // return dayjs(time).locale(locale === 'zh-Hans' ? 'zh-cn' : locale).fromNow()
+    }, [locale])
 
-  return { formatTimeFromNow }
+    return { formatTimeFromNow }
 }
